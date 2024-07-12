9ee4c670572dd0ef61f047e10b47980ff276da2b
Sanket Jadhav <sanketkj84@gmail.com>
2024-07-12 14:16:50 +0530

    Number Guessing Game

---
+++ Number_guess.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	int num,guess,tries=0;
+	cout<<"Number Guessing Game\n\n";
+	srand(time(0));
+	num=rand()%100+1;
+
+	do
+	{
+		cout<<"Enter a guess between 1 and 100: ";
+		cin>>guess;
+		tries++;
+
+		if(guess==num)
+			cout<<"\nCorrect! You got it in "<<tries<<" guesses!\n";
+		else if(guess>num && guess<=100)
+			cout<<"Too high!\n\n";
+		else if(guess<num && guess>=1)
+			cout<<"Too low!\n\n";
+		else
+			cout<<"Invalid Number\n\n";
+			
+	} while(guess!=num);
+
+	return 0;
+}
